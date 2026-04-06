c39374370b75bf53ba938110795142b7f712f806
gemondra-blip <gemondra@ucsc.edu>
2026-04-06 16:44:57 -0700

    Add files via upload
    Session 1 Prompt for ASTRO 19

---
+++ CodingJournal1.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr  1 21:20:09 2026
+
+@author: gabri
+"""
+s = 'Artemis 11'
+
+def Shi():
+    print('Hello my name is Gabriel Mondragon\n\nMy pronoun are He/Him\n\nI don\'t have a favorite movie but the best one I\'ve watched in recent menory is The Drama.\n\nFavorite food? I\'m really not a favorite\'s person, but lately I\'ve enjoyed green tea')
+    
+if __name__ == "__main__":
+    Shi()
